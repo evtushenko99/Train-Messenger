74dc7fa104a28382d88622946174932d93c1ece5
maxim <evtushenko99@mail.ru>
2019-09-25 18:10:27 +0300

    Use of intent

---
+++ app/src/main/java/com/example/train_messenger/ReceiveMessageActivity.java
@@ -0,0 +1,42 @@
+package com.example.train_messenger;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class ReceiveMessageActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView  = findViewById(R.id.receive_message);
+        messageView.setText(messageText);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
